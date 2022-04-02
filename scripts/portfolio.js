838a163ddf15c95e06375072588d19566560b621
mmatugas <102564061+hollyBelly2021@users.noreply.github.com>
2022-04-02 13:29:31 -0500

    update 3

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -15,8 +15,8 @@ document.getElementById(
 //ABOUT
 aboutButton.addEventListener("click", function () {
   document.getElementById("space").innerHTML = `
-  <h1 class="p-1 welcome animate__animated animate__fadeIn"> About Me </h1>
-  <p class='animate__animated animate__fadeIn animate__delay-10s p-3 text-justify '> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus elit, sollicitudin non tellus sed, vestibulum dictum risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam sed dolor sit amet sapien volutpat porta vel ut augue. Vestibulum eu orci magna. Maecenas posuere erat quis suscipit scelerisque. Curabitur hendrerit erat risus, at sagittis magna faucibus aliquam. Integer bibendum, augue at hendrerit blandit, tellus nisl pretium nunc, eget maximus neque odio vel lectus. Aliquam dolor quam, scelerisque vel faucibus at, convallis sit amet sem. Nam nec tortor tincidunt, dictum dolor eu, sodales mi. In eget facilisis nunc. Nullam in ex congue, dictum erat egestas, dignissim justo. Ut ultricies ornare mi, eget ornare nibh dapibus ut. Integer et accumsan dui.
+  <h1 class="display-4 p-1 welcome animate__animated animate__fadeIn text-center"> About Me </h1>
+  <p class='text-center animate__animated animate__fadeIn animate__delay-10s p-3 text-justify '> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus elit, sollicitudin non tellus sed, vestibulum dictum risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam sed dolor sit amet sapien volutpat porta vel ut augue. Vestibulum eu orci magna. Maecenas posuere erat quis suscipit scelerisque. Curabitur hendrerit erat risus, at sagittis magna faucibus aliquam. Integer bibendum, augue at hendrerit blandit, tellus nisl pretium nunc, eget maximus neque odio vel lectus. Aliquam dolor quam, scelerisque vel faucibus at, convallis sit amet sem. Nam nec tortor tincidunt, dictum dolor eu, sodales mi. In eget facilisis nunc. Nullam in ex congue, dictum erat egestas, dignissim justo. Ut ultricies ornare mi, eget ornare nibh dapibus ut. Integer et accumsan dui.
 
   </p>`;
 
@@ -27,12 +27,14 @@ aboutButton.addEventListener("click", function () {
 });
 
 contactButton.addEventListener("click", function () {
-  document.getElementById("space").innerHTML = `<div class="container">
-      <h1 class="p-2 welcome"> Contact Me </h1>
+  document.getElementById("space").innerHTML = `
+  
+      <h1 class="display-1 p-2 welcome text-center"> Contact Me </h1>
       
+      <div class="container">
       <form>  
           <div class="mb-3 row animate__animated animate__fadeInDown">
-              <label class=" col-sm-2 col-form-label"> Name: </label>
+              <label class="col-sm-2 col-form-label"> Name: </label>
               <div class="col-sm-9"> <input class="form-control" type="text" id="name" placeholder=""></div>
              
           </div>
@@ -51,10 +53,13 @@ contactButton.addEventListener("click", function () {
               <div class="col-sm-9">  <textarea class="form-control" rows="3" id="message" placeholder=""> </textarea></div>
              
           </div>
-          <button type="submit" class="btn btn-outline-secondary text-light animate__animated animate__fadeInDown animate__delay-4s"> submit </button>
+          <div class="d-flex flex-column align-items-center">
+          <button type="submit" class=" text-center btn btn-outline-secondary text-light animate__animated animate__fadeInDown animate__delay-4s"> submit </button>
+          </div>
       </form>
+    </div>
 
-  </div>`;
+  `;
 });
 
 projectButton.addEventListener("click", function () {
